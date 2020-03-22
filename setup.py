1d9f4951b77a189ddc9e4728fb9db49f6fcbb4cc
kun1989 <15828528791@163.com>
2020-03-22 16:36:30 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="pth-easy",
+    version="0.1",
+    author="xqk",
+    description="some useful code for pytorch",
+    packages=find_packages(),
+)
